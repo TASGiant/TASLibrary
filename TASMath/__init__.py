22275b1b6d96ff9cef279cbe5c3895a58f4a8066
TASGiant <90855294+TASGiant@users.noreply.github.com>
2021-10-08 21:30:30 -0500

    Create __init__.py

---
+++ TASMath/__init__.py
@@ -0,0 +1,63 @@
+"""
+Pakage Name : TASMath
+Total Modiul : 8
+Modiul Name : [Alzebra, Arithmetic, Constant, Convert, ExtraFunction, Geometry, Series, UnitPrefix]
+Author : TAS Giant
+Virsion : 1.0
+"""
+import Alzebra
+"""
+Modiul Name : Alzebra
+Total Class : 5
+Class Name : [Square, Cube, Power, ProducerFormula, Print]
+Square total Function : 3
+Square Function Name : [square, singleSquare, square_]
+Square Sub Class: 1
+Square Sub Class Name : [Extended]
+Square Sub Class Function : 1
+Square Sub Class Function Name : [Square]
+Cube total Function : 2
+Cube Function Name : [Cube, Cube_]
+Power total Function : 1
+Power Function : [Power]
+ProduserFormula total Function : 2
+ProduserFormula Function : [a2intersectionb2, a3addb3]
+Print total Function : None
+Print Sub Class : 3
+Print Sub Class Name : [Square, Cube, Power]
+"""
+import Arithmetic
+"""
+Modiul Name : Arithmetic
+Total Function : 5
+Function Name : [add, substract, devide, multiply, reminder]
+Total Class : 1
+Class Name : Print
+"""
+import Constant
+"""
+Modiul Name : Constant
+Total Function : 10
+Function Name : [pi, pi_25, pi_50, pi_100, current, PlanckConstant, ChargeOfElectrons, BoltzmannConstant, AvogadroConstant,
+RadiationIntensity]
+Total Class : 1
+Class Name : Number
+Class Function : 10
+Class Function Name == Function Name
+Sub Class : 1
+Sub Class Name : Print
+Sub Class Function : 10
+Sub Class Function Name == Function Name
+"""
+import Convert
+import ExtraFunction
+import Geometry
+import Series
+import UnitPrefix
+
+__virsion__ = '0.1.0'
+__author__ = 'TAS Giant'
+__name__ = 'TASMath'
+
+__all__ = ["Alzebra", "Arithmetic", "Constant", "Convert", "ExtraFunction", "Geometry",
+           "Series", "UnitPrefix","__virsion__", "__author__",  "__name__"]
