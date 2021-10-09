7d0ea75c59c7cdbecb8255eb224aa40e8b13a02d
TAS Giant <90855294+TASGiant@users.noreply.github.com>
2021-10-08 22:16:04 -0500

    Create UnitPrefix.py

---
+++ TASPhycies/UnitPrefix.py
@@ -0,0 +1,183 @@
+def Axa():
+    return 10 ** 18
+
+
+def Peta():
+    return 10 ** 15
+
+
+def Tera():
+    return 10 ** 12
+
+
+def Mega():
+    return 10 ** 6
+
+
+def Kilo():
+    return 10 ** 3
+
+
+def Hecto():
+    return 10 ** 2
+
+
+def Deka():
+    return 10 ** 1
+
+
+def Daisy():
+    return 10 ** (-1)
+
+
+def Cm():
+    return 10 ** (-2)
+
+
+def Mili():
+    return 10 ** (-3)
+
+
+def Micro():
+    return 10 ** (-6)
+
+
+def Nano():
+    return 10 ** (-9)
+
+
+def Pico():
+    return 10 ** (-12)
+
+
+def Femto():
+    return 10 ** (-15)
+
+
+def Eto():
+    return 10 ** (-18)
+
+
+class Number:
+
+    @staticmethod
+    def Axa(number):
+        return number * (10 ** 18)
+
+    @staticmethod
+    def Peta(number):
+        return number * (10 ** 15)
+
+    @staticmethod
+    def Tera(number):
+        return number * (10 ** 12)
+
+    @staticmethod
+    def Mega(number):
+        return number * (10 ** 6)
+
+    @staticmethod
+    def Kilo(number):
+        return number * (10 ** 3)
+
+    @staticmethod
+    def Hecto(number):
+        return number * (10 ** 2)
+
+    @staticmethod
+    def Deka(number):
+        return number * (10 ** 1)
+
+    @staticmethod
+    def Daisy(number):
+        return number * (10 ** (-1))
+
+    @staticmethod
+    def Cm(number):
+        return number * (10 ** (-2))
+
+    @staticmethod
+    def Mili(number):
+        return number * (10 ** (-3))
+
+    @staticmethod
+    def Micro(number):
+        return number * (10 ** (-6))
+
+    @staticmethod
+    def Nano(number):
+        return number * (10 ** (-9))
+
+    @staticmethod
+    def Pico(number):
+        return number * (10 ** (-12))
+
+    @staticmethod
+    def Femto(number):
+        return number * (10 ** (-15))
+
+    @staticmethod
+    def Eto(number):
+        return number * (10 ** (-18))
+
+    class Print:
+
+        @staticmethod
+        def Axa(number):
+            print(number * (10 ** 18))
+
+        @staticmethod
+        def Peta(number):
+            print(number * (10 ** 15))
+
+        @staticmethod
+        def Tera(number):
+            print(number * (10 ** 12))
+
+        @staticmethod
+        def Mega(number):
+            print(number * (10 ** 6))
+
+        @staticmethod
+        def Kilo(number):
+            print(number * (10 ** 3))
+
+        @staticmethod
+        def Hecto(number):
+            print(number * (10 ** 2))
+
+        @staticmethod
+        def Deka(number):
+            print(number * (10 ** 1))
+
+        @staticmethod
+        def Daisy(number):
+            print(number * (10 ** (-1)))
+
+        @staticmethod
+        def Cm(number):
+            print(number * (10 ** (-2)))
+
+        @staticmethod
+        def Mili(number):
+            print(number * (10 ** (-3)))
+
+        @staticmethod
+        def Micro(number):
+            print(number * (10 ** (-6)))
+
+        @staticmethod
+        def Nano(number):
+            print(number * (10 ** (-9)))
+
+        @staticmethod
+        def Pico(number):
+            print(number * (10 ** (-12)))
+
+        @staticmethod
+        def Femto(number):
+            print(number * (10 ** (-15)))
+
+        @staticmethod
+        def Eto(number):
+            print(number * (10 ** (-18)))
